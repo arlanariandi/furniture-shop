9a847e36f2fe7cf7ca5204c89c89bc9d09f2b030
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2024-06-15 16:19:05 +0700

    UPDATE: Blade template

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\FrontendController::class, 'index'])->name('index');
+Route::get('/details/{slug}', [\App\Http\Controllers\FrontendController::class, 'details'])->name('details');
+Route::get('/cart', [\App\Http\Controllers\FrontendController::class, 'cart'])->name('cart');
+Route::get('/checkout/success', [\App\Http\Controllers\FrontendController::class, 'success'])->name('checkout-success');
 
 Route::middleware([
     'auth:sanctum',
